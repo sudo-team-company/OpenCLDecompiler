755ef8d6d8f89605dace0afc5cd85ceab01c7d5e
RubyUmbra <rubyumbra@gmail.com>
2022-02-13 19:37:26 +0300

    Update global_store instruction implementation.
    Implemented only for saddr == 'off' where it works same as flat_store.

--- src/instructions/flat/global_store.py
+++ src/instructions/flat/global_store.py
@@ -1,4 +1,7 @@
 from src.base_instruction import BaseInstruction
+from src.decompiler_data import make_elem_from_addr, make_new_type_without_modifier
+from src.opencl_types import make_opencl_type
+from src.register import check_and_split_regs, is_vgpr
 
 
 class GlobalStore(BaseInstruction):
@@ -6,8 +9,16 @@ class GlobalStore(BaseInstruction):
         super().__init__(node, suffix)
         self.vaddr = self.instruction[1]
         self.vdata = self.instruction[2]
-        self.saddr = "0" if self.instruction[3] == "off" else self.instruction[3]
+        if self.instruction[3] != "off":
+            raise NotImplementedError(
+                f"Only 'off' as saddr implemented for global_store instructions. "
+                f"Current instruction: '{' '.join(self.instruction)}'"
+            )
+        self.saddr = "0"
         self.inst_offset = "0" if len(self.instruction) == 4 else self.instruction[4]
+
+        self.to_registers, _ = check_and_split_regs(self.vaddr)
+        self.from_registers, self.from_registers_1 = check_and_split_regs(self.vdata)
 
     def to_print_unresolved(self):
         if self.suffix == "dword":
@@ -19,3 +30,58 @@ class GlobalStore(BaseInstruction):
                                        + ") = " + self.vdata + " // global_store_dwordx2\n")
             return self.node
         return super().to_print_unresolved()
+
+    def to_fill_node(self):
+        if self.suffix in ["dword", "dwordx2"]:
+            if is_vgpr(self.vaddr):
+                self.node.state.registers[self.to_registers].version = \
+                    self.node.parent[0].state.registers[self.to_registers].version
+                self.node.state.registers[self.to_registers].data_type = self.suffix
+            # TODO: Сделать присвоение в пары
+            else:
+                if self.node.state.registers[self.from_registers].data_type is not None \
+                        and 'bytes' in self.node.state.registers[self.from_registers].data_type:
+                    self.node.state.registers[self.from_registers].data_type = \
+                        self.node.state.registers[self.to_registers].data_type
+                    self.decompiler_data.names_of_vars[self.node.state.registers[self.from_registers].val] = \
+                        self.node.state.registers[self.to_registers].data_type
+                else:
+                    if str(self.node.state.registers[self.from_registers].data_type) not in \
+                            self.node.state.registers[self.to_registers].data_type:
+                        if self.node.state.registers[self.from_registers].val in self.decompiler_data.names_of_vars:
+                            val = self.node.state.registers[self.from_registers].val
+                            self.node.state.registers[self.from_registers].val = \
+                                '(' + make_opencl_type(
+                                    make_new_type_without_modifier(self.node, self.to_registers)) + ')' \
+                                + self.node.state.registers[self.from_registers].val
+                            # init var - i32, gdata - i64. var = gdata -> var - i64
+                            self.decompiler_data.names_of_vars[val] = \
+                                self.node.state.registers[self.from_registers].data_type
+                        else:
+                            self.node.state.registers[self.from_registers].data_type = \
+                                make_new_type_without_modifier(self.node, self.to_registers)
+                            self.decompiler_data.names_of_vars[self.node.state.registers[self.from_registers].val] = \
+                                self.node.state.registers[self.from_registers].data_type
+            return self.node
+        return super().to_fill_node()
+
+    def to_print(self):
+        if self.suffix in ["dword", "dwordx2"]:
+            var = self.node.state.registers[self.to_registers].val
+            if " + " in var:
+                var = make_elem_from_addr(var)
+            else:
+                if self.decompiler_data.names_of_vars.get(var):
+                    var = "*(" + make_opencl_type(self.decompiler_data.names_of_vars[var]) + "*)(" + var + ")"
+                else:
+                    var = "*" + var
+            if self.node.state.registers.get(self.from_registers):
+                if self.node.state.registers[self.from_registers].val == "0" \
+                        and self.node.state.registers.get(self.from_registers_1):
+                    self.output_string = var + " = " + self.node.state.registers[self.from_registers_1].val
+                else:
+                    self.output_string = var + " = " + self.node.state.registers[self.from_registers].val
+            else:
+                self.output_string = var + " = " + self.decompiler_data.initial_state.registers[self.from_registers].val
+            return self.output_string
+        return super().to_print()
