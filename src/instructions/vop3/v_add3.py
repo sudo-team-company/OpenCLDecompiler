1e94378c3f54895fd8ca6c32bc68169bbcf716b8
RubyUmbra <rubyumbra@gmail.com>
2022-02-20 19:59:09 +0300

    Refactoring of v_add3

--- src/instructions/vop3/v_add3.py
+++ src/instructions/vop3/v_add3.py
@@ -5,21 +5,21 @@ from src.register import is_reg
 from src.register_type import RegisterType
 
 _types_of_sum_for_global_id = {
-    RegisterType.GLOBAL_ID_X: {
+    frozenset({
         RegisterType.GLOBAL_OFFSET_X,
         RegisterType.WORK_ITEM_ID_X,
         RegisterType.WORK_GROUP_ID_X_LOCAL_SIZE,
-    },
-    RegisterType.GLOBAL_ID_Y: {
+    }): ("get_global_id(0)", RegisterType.GLOBAL_ID_X),
+    frozenset({
         RegisterType.GLOBAL_OFFSET_Y,
         RegisterType.WORK_ITEM_ID_Y,
         RegisterType.WORK_GROUP_ID_Y_LOCAL_SIZE,
-    },
-    RegisterType.GLOBAL_ID_Z: {
+    }): ("get_global_id(1)", RegisterType.GLOBAL_ID_Y),
+    frozenset({
         RegisterType.GLOBAL_OFFSET_Z,
         RegisterType.WORK_ITEM_ID_Z,
         RegisterType.WORK_GROUP_ID_Z_LOCAL_SIZE,
-    },
+    }): ("get_global_id(2)", RegisterType.GLOBAL_ID_Z),
 }
 
 
@@ -44,23 +44,23 @@ class VAdd3(BaseInstruction):
 
     def to_fill_node(self):
         if self.suffix == 'u32':
-            src0_reg = is_reg(self.src0)
-            src1_reg = is_reg(self.src1)
-            src2_reg = is_reg(self.src2)
-            if src0_reg and src1_reg and src2_reg:
-                src_types = {
+            if is_reg(self.src0) and is_reg(self.src1) and is_reg(self.src2):
+                src_types = frozenset({
                     self.node.state.registers[self.src0].type,
                     self.node.state.registers[self.src1].type,
                     self.node.state.registers[self.src2].type,
-                }
-                data_type = self.suffix
-                reg_entire = Integrity.ENTIRE
+                })
 
-                for i, reg_type in enumerate(_types_of_sum_for_global_id):
-                    if src_types == _types_of_sum_for_global_id[reg_type]:
-                        return set_reg_value(self.node, f"get_global_id({i})", self.vdst,
-                                             [self.src0, self.src1], data_type,
-                                             reg_type=reg_type,
-                                             reg_entire=reg_entire)
+                if src_types in _types_of_sum_for_global_id:
+                    new_value, reg_type = _types_of_sum_for_global_id[src_types]
+                    return set_reg_value(
+                        node=self.node,
+                        new_value=new_value,
+                        to_reg=self.vdst,
+                        from_regs=[self.src0, self.src1, self.src2],
+                        data_type=self.suffix,
+                        reg_type=reg_type,
+                        reg_entire=Integrity.ENTIRE,
+                    )
 
         return super().to_fill_node()
