54a6712a08da11f40c30e654578abe11e1e83299
RubyUmbra <rubyumbra@gmail.com>
2022-02-26 22:09:53 +0300

    Add "copy_get_num_groups" test for gfx1010 and gfx gfx1030

--- tests/linear_kernels_test.py
+++ tests/linear_kernels_test.py
@@ -44,8 +44,9 @@ class TestLinearKernels:
     def test_copy_get_global_size(self, mcpu):
         template('linear_kernels/work_item_built_in_functions', 'copy_get_global_size', mcpu=mcpu)
 
-    def test_copy_get_num_groups(self):
-        template('linear_kernels/work_item_built_in_functions', 'copy_get_num_groups')
+    @pytest.mark.parametrize("mcpu", ["", "gfx1010", "gfx1030"])
+    def test_copy_get_num_groups(self, mcpu):
+        template('linear_kernels/work_item_built_in_functions', 'copy_get_num_groups', mcpu=mcpu)
 
     def test_copy_get_work_dim(self):
         template('linear_kernels/work_item_built_in_functions', 'copy_get_work_dim')
