7c72bec81d9854df2af8438a0225cfc5ff2e42a6
RubyUmbra <rubyumbra@gmail.com>
2024-04-16 17:45:21 +0300

    Fix error: implement `s_not` instruction processing

--- src/instructions/sop1/s_not.py
+++ src/instructions/sop1/s_not.py
@@ -1,4 +1,5 @@
 from src.base_instruction import BaseInstruction
+from src.decompiler_data import set_reg_value
 
 
 class SNot(BaseInstruction):
@@ -8,8 +9,16 @@ class SNot(BaseInstruction):
         self.ssrc0 = self.instruction[2]
 
     def to_print_unresolved(self):
-        if self.suffix == 'b64':
-            self.decompiler_data.write(self.sdst + " = ~" + self.ssrc0 + " // s_not_b64\n")
+        if self.suffix in ['b32', 'b64']:
+            self.decompiler_data.write(self.sdst + " = ~" + self.ssrc0 + f" // s_not_{self.suffix}\n")
             self.decompiler_data.write("scc = " + self.sdst + " != 0\n")
             return self.node
         return super().to_print_unresolved()
+
+    def to_fill_node(self):
+        if self.suffix in ['b32', 'b64']:
+            new_value = "!(" + self.node.state.registers[self.ssrc0].val + ")"
+            data_type = self.suffix
+            reg_type = self.node.state.registers[self.ssrc0].type
+            return set_reg_value(self.node, new_value, self.sdst, [self.ssrc0], data_type, reg_type=reg_type)
+        return super().to_fill_node()
