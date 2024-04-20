da3453d0cdfc01fa8b13a5f068c87447c36218cf
RubyUmbra <rubyumbra@gmail.com>
2024-04-20 18:01:56 +0300

    Add tests for loops for RDNA

--- tests/loops_kernels_test.py
+++ tests/loops_kernels_test.py
@@ -15,3 +15,7 @@ class TestLoopsKernels:
     @pytest.mark.parametrize("mcpu", ["gfx1010", "gfx1030"])
     def test_loop_general(self, mcpu):
         template('loops_kernels', 'loop_general', mcpu=mcpu)
+
+    @pytest.mark.parametrize("mcpu", ["gfx1010", "gfx1030"])
+    def test_loop_with_unrolling_breaker(self, mcpu):
+        template('loops_kernels', 'loop_with_unrolling_breaker', mcpu=mcpu)
