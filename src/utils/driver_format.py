bb313c0a9affc46a0a444c1eeae0b38b7cf495c8
RubyUmbra <rubyumbra@gmail.com>
2024-10-17 17:12:41 +0300

    Fix input file parser selection

--- src/utils/driver_format.py
+++ src/utils/driver_format.py
@@ -10,10 +10,8 @@ class DriverFormat(Enum):
     @classmethod
     def _missing_(cls, value):
         if isinstance(value, list):
-            try:
-                return DriverFormat(value[1])
-            except ValueError:
-                pass
+            if (res := DriverFormat(value[1])) != DriverFormat.UNKNOWN:
+                return res
             if '\t.amdgpu_pal_metadata' in value:
                 return DriverFormat.AMDGPU_DIS
         return DriverFormat.UNKNOWN
