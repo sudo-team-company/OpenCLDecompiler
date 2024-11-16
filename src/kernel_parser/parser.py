e47a19ef3c10b383843362aca99eb5efac47ca52
RubyUmbra <rubyumbra@gmail.com>
2024-11-16 13:59:14 +0300

    Remove unnecessary `DriverFormat` class

--- src/kernel_parser/parser.py
+++ src/kernel_parser/parser.py
@@ -1,12 +1,14 @@
 from . import amdcl2_parser, rocm_parser, amdgpu_dis_parser, unknown_parser
-from ..utils import DriverFormat
 
-parsers = {
-    DriverFormat.AMDCL2: amdcl2_parser,
-    DriverFormat.ROCM: rocm_parser,
-    DriverFormat.AMDGPU_DIS: amdgpu_dis_parser,
-    DriverFormat.UNKNOWN: unknown_parser,
-}
+
+def _choose_parser(text: list[str]):
+    if '.amdcl2' in text:
+        return amdcl2_parser
+    if '.rocm' in text:
+        return rocm_parser
+    if '\t.amdgpu_pal_metadata' in text:
+        return amdgpu_dis_parser
+    return unknown_parser
 
 
 def parse_gpu(text: list[str]) -> str | None:
@@ -16,7 +18,6 @@ def parse_gpu(text: list[str]) -> str | None:
     return None
 
 
-def parse_kernel(text):
-    driver_format = DriverFormat(text)
+def parse_kernel(text: list[str]):
     gpu = parse_gpu(text)
-    return parsers[driver_format].parse_kernel(text), gpu
+    return _choose_parser(text).parse_kernel(text), gpu
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,4 @@
 from .config_data import ConfigData
-from .driver_format import DriverFormat
 from .singleton import Singleton
 from .global_context import get_context
 from .kernel_argument import KernelArgument
@@ -7,7 +6,6 @@ from .uuid import generate_uuid
 
 __all__ = [
     "ConfigData",
-    "DriverFormat",
     "Singleton",
     "get_context",
     "generate_uuid",
