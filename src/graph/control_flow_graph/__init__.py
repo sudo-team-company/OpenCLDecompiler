7e9daccb22eae0b98c0a56a32e8f7f4208ccf69f
RubyUmbra <rubyumbra@gmail.com>
2024-11-25 19:51:27 +0300

    Fix F403 [undefined-local-with-import-star] warning

--- src/graph/control_flow_graph/__init__.py
+++ src/graph/control_flow_graph/__init__.py
@@ -1,6 +1,7 @@
-from .constants import *
+from .constants import CONTROL_FLOW_GRAPH_ENABLED_CONTEXT_KEY
 from .control_flow_graph import ControlFlowGraph
 
 __all__ = [
+    "CONTROL_FLOW_GRAPH_ENABLED_CONTEXT_KEY",
     "ControlFlowGraph",
 ]
