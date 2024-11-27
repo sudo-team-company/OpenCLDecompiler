c58a745e1560559ef3f295f094d585af95fce739
RubyUmbra <rubyumbra@gmail.com>
2024-11-27 22:55:00 +0300

    Fix ISC001 [single-line-implicit-string-concatenation] warning

--- src/utils/uuid.py
+++ src/utils/uuid.py
@@ -5,4 +5,4 @@ def generate_uuid(
     prefix: str | None = None,
     suffix: str | None = None,
 ) -> str:
-    return f"{'' if not prefix else prefix}" f"{uuid.uuid4()!s}" f"{'' if not suffix else suffix}"
+    return f"{'' if not prefix else prefix}{uuid.uuid4()!s}{'' if not suffix else suffix}"
