488b0228437f1e26d1436b3d6add5e14ff5dfb49
RubyUmbra <rubyumbra@gmail.com>
2024-11-28 19:57:50 +0300

    Fix SIM212 [if-expr-with-twisted-arms] warning

--- src/utils/uuid.py
+++ src/utils/uuid.py
@@ -5,4 +5,4 @@ def generate_uuid(
     prefix: str | None = None,
     suffix: str | None = None,
 ) -> str:
-    return f"{'' if not prefix else prefix}{uuid.uuid4()!s}{'' if not suffix else suffix}"
+    return f"{prefix if prefix else ''}{uuid.uuid4()!s}{suffix if suffix else ''}"
