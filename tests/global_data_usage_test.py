87caf55e103344b979ff94457b4897e93fca91bc
RubyUmbra <rubyumbra@gmail.com>
2025-09-17 14:54:31 +0300

    Revert "Add global_data_usage test cases"
    This reverts commit 7e33130cd3dffcc24e59948ce5fe81b8c033b52c.

--- tests/global_data_usage_test.py
+++ tests/global_data_usage_test.py
@@ -8,8 +8,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_int_array(self, mcpu, disasm):
@@ -24,8 +22,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_long_array(self, mcpu, disasm):
@@ -40,8 +36,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_mixed_array(self, mcpu, disasm):
@@ -56,8 +50,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_float_array(self, mcpu, disasm):
@@ -72,8 +64,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_double_array(self, mcpu, disasm):
@@ -88,8 +78,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_cvt_i32_f32(self, mcpu, disasm):
@@ -104,8 +92,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_cvt_f32_i32(self, mcpu, disasm):
@@ -120,8 +106,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_cvt_i32_f64(self, mcpu, disasm):
@@ -136,8 +120,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_cvt_f64_i32(self, mcpu, disasm):
@@ -148,28 +130,24 @@ class TestGlobalData:
             disasm=disasm,
         )
 
-    @pytest.mark.parametrize(
-        ["mcpu", "disasm"],
-        [
-            ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
-        ],
-    )
-    def test_cvt_f64_i64(self, mcpu, disasm):
-        template(
-            path_to_dir='global_data_usage',
-            dir_name='cvt_f64_i64',
-            mcpu=mcpu,
-            disasm=disasm,
-        )
+    # @pytest.mark.parametrize(
+    #     ["mcpu", "disasm"],
+    #     [
+    #         ("amd_gcn", "clrxdisasm"),
+    #     ],
+    # )
+    # def test_cvt_f64_i64(self, mcpu, disasm):
+    #     template(
+    #         path_to_dir='global_data_usage',
+    #         dir_name='cvt_f64_i64',
+    #         mcpu=mcpu,
+    #         disasm=disasm,
+    #     )
 
     @pytest.mark.parametrize(
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_int4(self, mcpu, disasm):
@@ -184,8 +162,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_int8(self, mcpu, disasm):
@@ -200,8 +176,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_mix_vectors(self, mcpu, disasm):
@@ -216,8 +190,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_int_matrix(self, mcpu, disasm):
@@ -232,8 +204,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_long_matrix(self, mcpu, disasm):
@@ -248,8 +218,6 @@ class TestGlobalData:
         ["mcpu", "disasm"],
         [
             ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
         ],
     )
     def test_int2_array(self, mcpu, disasm):
@@ -260,50 +228,44 @@ class TestGlobalData:
             disasm=disasm,
         )
 
-    @pytest.mark.parametrize(
-        ["mcpu", "disasm"],
-        [
-            ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
-        ],
-    )
-    def test_int2_to_int2(self, mcpu, disasm):
-        template(
-            path_to_dir='global_data_usage',
-            dir_name='int2_to_int2',
-            mcpu=mcpu,
-            disasm=disasm,
-        )
+    # @pytest.mark.parametrize(
+    #     ["mcpu", "disasm"],
+    #     [
+    #         ("amd_gcn", "clrxdisasm"),
+    #     ],
+    # )
+    # def test_int2_to_int2(self, mcpu, disasm):
+    #     template(
+    #         path_to_dir='global_data_usage',
+    #         dir_name='int2_to_int2',
+    #         mcpu=mcpu,
+    #         disasm=disasm,
+    #     )
 
-    @pytest.mark.parametrize(
-        ["mcpu", "disasm"],
-        [
-            ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
-        ],
-    )
-    def test_int4_to_int4(self, mcpu, disasm):
-        template(
-            path_to_dir='global_data_usage',
-            dir_name='int4_to_int4',
-            mcpu=mcpu,
-            disasm=disasm,
-        )
+    # @pytest.mark.parametrize(
+    #     ["mcpu", "disasm"],
+    #     [
+    #         ("amd_gcn", "clrxdisasm"),
+    #     ],
+    # )
+    # def test_int4_to_int4(self, mcpu, disasm):
+    #     template(
+    #         path_to_dir='global_data_usage',
+    #         dir_name='int4_to_int4',
+    #         mcpu=mcpu,
+    #         disasm=disasm,
+    #     )
 
-    @pytest.mark.parametrize(
-        ["mcpu", "disasm"],
-        [
-            ("amd_gcn", "clrxdisasm"),
-            ("gfx1010", "amdgpu-dis"),
-            ("gfx1030", "amdgpu-dis"),
-        ],
-    )
-    def test_int8_to_int8(self, mcpu, disasm):
-        template(
-            path_to_dir='global_data_usage',
-            dir_name='int8_to_int8',
-            mcpu=mcpu,
-            disasm=disasm,
-        )
+    # @pytest.mark.parametrize(
+    #     ["mcpu", "disasm"],
+    #     [
+    #         ("amd_gcn", "clrxdisasm"),
+    #     ],
+    # )
+    # def test_int8_to_int8(self, mcpu, disasm):
+    #     template(
+    #         path_to_dir='global_data_usage',
+    #         dir_name='int8_to_int8',
+    #         mcpu=mcpu,
+    #         disasm=disasm,
+    #     )
