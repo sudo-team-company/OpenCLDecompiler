0577ab64e288175117e9de3e27a4e5afdd7b684f
Fyodor Gorelov <fvgorelov@gmail.com>
2026-05-15 12:53:42 +0300

    реализация дополнительных обобщающих инструкций

---
+++ src/ir/instructions/special/mask.py
@@ -0,0 +1,47 @@
+from src.ir.instructions.generic import GenericInstruction
+from src.ir.registers.reg import PredReg
+from src.ir.instructions.lowering import NodeLoweringContext
+from src.instructions.IRspecial.ChangeMask import UseMask, Unmask as be_Unmask
+
+
+class ChangeMask(GenericInstruction):
+    def __init__(self, predicate: PredReg):
+        super().__init__("change_mask", predicate)
+        self.predicate = predicate
+
+    def _get_normalize_opcode(self):
+        return "change_mask"
+
+    def to_fill_node(self, state, parents):
+        return NodeLoweringContext(state, parents).emit_backend(
+            UseMask,
+            self._get_normalize_opcode(),
+            self.operands,
+        )
+    
+    def writes_first_operand(self) -> bool:
+        return False
+
+    def has_side_effects(self) -> bool:
+        return True
+
+
+class Unmask(GenericInstruction):
+    def __init__(self):
+        super().__init__("unmask")
+
+    def _get_normalize_opcode(self):
+        return "unmask"
+
+    def to_fill_node(self, state, parents):
+        return NodeLoweringContext(state, parents).emit_backend(
+            be_Unmask,
+            self._get_normalize_opcode(),
+            [],
+        )
+
+    def writes_first_operand(self) -> bool:
+        return False
+
+    def has_side_effects(self) -> bool:
+        return True
---
+++ src/ir/instructions/special/memory.py
@@ -0,0 +1,44 @@
+from src.ir.instructions.generic import GenericInstruction
+from src.ir.registers.reg import Reg64, Reg_ty, Val
+
+
+from src.ir.instructions.lowering import NodeLoweringContext
+from src.instructions.IRspecial.memory import MemoryAllocation as DecMA, StoreInMem
+
+class MemoryAllocation(GenericInstruction):
+    def __init__(self, destination: Reg64):
+        super().__init__("alloc", destination)
+        
+    def _get_normalize_opcode(self) -> str:
+        return "s_alloc"
+
+    def to_fill_node(self, state, parents):
+        return NodeLoweringContext(state, parents).emit_backend(
+            DecMA,
+            self._get_normalize_opcode(),
+            self.operands,
+        )
+
+
+class Store(GenericInstruction):
+    def __init__(self, destination: Reg_ty, arg_name: Val, arg_type: Val, offset: Val):
+        super().__init__("store", destination, arg_type, arg_name, offset)
+        self.destination = destination
+        self.arg_name = arg_name
+        self.arg_type = arg_type
+        self.offset = offset
+
+    def _get_normalize_opcode(self) -> str:
+        return "s_store"
+    
+    def set_arg_type(self, type_name: str) -> None:
+        self.arg_type = Val(type_name)
+        self.operands = (self.destination, self.arg_type, self.arg_name, self.offset)
+
+
+    def to_fill_node(self, state, parents):
+        return NodeLoweringContext(state, parents).emit_backend(
+            StoreInMem,
+            self._get_normalize_opcode(),
+            self.operands,
+        )
